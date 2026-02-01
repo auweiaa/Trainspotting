846a1b9ebbc465458d98c64a64f4e6078f22957c
auweiaa <tim.hunker@hhu.de>
2026-02-01 23:55:52 +0100

    worked on new classes and commented on the code so that following my thoughts is maybe a little easier :D

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,4 +1,6 @@
 import TSim.*;
+
+import java.util.Objects;
 import java.util.concurrent.Semaphore;
 
 // Switches bei:
@@ -17,8 +19,13 @@ public class Lab1 {
       int currentSpeed = speed1;
     //  tsi.setSpeed(2, speed2);
 
+
+
       try {
         while (true) {
+
+          // -----------------------------------------------------------------------
+          // First Test -> will be replaced after all Classes are created
           SensorEvent event = tsi.getSensor(1);
           int dir = (int) Math.signum(currentSpeed);
 
@@ -56,7 +63,11 @@ public class Lab1 {
               
             }
           }
+
+          // -----------------------------------------------------------------------
+
         }
+
       } catch (Exception e) {
         e.printStackTrace();
       }
@@ -68,11 +79,14 @@ public class Lab1 {
   }
 }
 
+// ------------------------------------------------------------------------
+// Classes for better Handling the trains (instead of using too much if-else)
+
 enum TrainMode {
   RUNNING_FORWARD, RUNNING_BACKWARD, WAITING_AT_STATION //,WAITING_FOR_OVERTAKE
 } 
 
-
+// not sure if needed like that. We will decide later :)
 class TrainState {
   private final int trainId;
   private int currentSpeed;
@@ -84,7 +98,6 @@ class TrainState {
     this.mode = mode;
   }
 
-  // getter
   public int getTrainId() {
     return this.trainId;
   }
@@ -97,7 +110,6 @@ class TrainState {
     return this.mode;
   }
 
-  //setter
   public void setMode(TrainMode mode ){
     this.mode = mode;
   }
@@ -107,11 +119,7 @@ class TrainState {
   } 
 }
 
-
-/* 
-  Pos (Koordinate als Key für Regel-Liste/Map)
-  equals/hashCode um Regeln wiederzufinden
- */
+// class for Position in the map. Either Sensor or Switch
 class Position {
   private int xPos;
   private int yPos;
@@ -121,61 +129,120 @@ class Position {
     this.yPos = y;
   }
 
-  @Override
-  public boolean equals(Object pos) {
-
-    // 
-    return true;
+  public int getXpos() {
+    return this.xPos;
+  }
+  
+  public int getYpos() {
+    return this.yPos;
   }
 
   @Override
+  public boolean equals(Object obj) {
+
+    if (this == obj) return true;
+
+    if (obj == null) return false;
+      
+    if (getClass() != obj.getClass()) return false;
+
+    Position other = (Position) obj;
+    return xPos == other.xPos && yPos == other.yPos;
+  }  
+
+  @Override
   public int hashCode(){
-    //
-    return 0;
+    return Objects.hash(xPos, yPos);
   }
 
 }
 
+// just for the type security, not necessary but nice to have. could be deleted if you want
+// reason: with that you cant compile the code while setting a switch with invalid direction
+// because valid directions are hex:0x01, 0x02 / int: 1, 2 (left, right)
+enum SwitchDirection {
+  LEFT(TSimInterface.SWITCH_LEFT), 
+  RIGHT(TSimInterface.SWITCH_RIGHT);
+  
+  private final int tsimValue;
+  
+  SwitchDirection(int tsimValue) {
+    this.tsimValue = tsimValue;
+  }
+  
+  public int toTsimDirection() {
+    return this.tsimValue;
+  }
+}
 
-
-/*
-  Statt if verzweigungen, besser Liste/Map um Regeln nachzuschlagen:
-
-  Map<Pos, List<Rule>> rulesBySensor
-  Lookup-Flow im Eventloop:
-  event = getSensor(trainId)
-  Nur wenn event.status == ACTIVE
-  pos = (event.x, event.y)
-  rules = rulesBySensor.get(pos)
-  Falls rules != null: in Reihenfolge prüfen und die erste passende anwenden
-*/
-
-
-
+// TODO:
 /* 
-  SwitchRule für RUNNING_FORWARD oder RUNNING_BACKWARD
+  SwitchRule für RUNNING_FORWARD oder RUNNING_BACKWARD (almost done)
   StationRule für RUNNING_FORWARD oder RUNNING_BACKWARD
   OvertakeRule
  */
 
 
+// ToDo: in switching Rule: stopping the train and setSpeed again
+// -> TrainController as instance is needed for that
+// -> passing it as a parameter to the method executeRule
+
+interface Rule {
+  public void executeRule();
+}
+
+class SwitchRule implements Rule {
+
+  private final Position position;
+  private final SwitchDirection direction;
+  
+  public SwitchRule(Position position, SwitchDirection direction) {
+    this.position = position;
+    this.direction = direction;
+  }
+
+  @Override
+  public void executeRule() {
+      TSimInterface tsim = TSimInterface.getInstance();
+
+      try {
+        tsim.setSwitch(position.getXpos(), position.getYpos(), direction.toTsimDirection());
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+  }
+}
+
+
+// next steps: Dispatcher Class for Rule Map to look up which Rule is needed in which constellation
+
+
+// Todo: finish map so that the dispathcer class can make look ups / decide what when to do.
 class TrainController implements Runnable {
   private final int trainId;
-  private TrainState state;
+  private TrainState trainState;
   private Map<Position, List<Rule>> ruleMap;
 
 
   public TrainController(int id, TrainState state, Map<Position, List<Rule>> ruleMap) {
     this.trainId = id;
-    this.state = state;
+    this.trainState = state;
     this.ruleMap = ruleMap;
   }
 
-  //getter
+  public int getTrainId() {
+    return this.trainId;
+  }
 
-  //setter
+  public TrainState getTrainState() {
+    return this.trainState;
+  }
 
-  // implementing methods from runnable
+  public Rule findMatchinRule() {
+    return null;
+  }
+
+  // implementing methods from runnable!!
 
 
 }
