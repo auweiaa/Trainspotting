45f0e5d95cb2d53610dc239331e0c0e5479f48ff
auweiaa <tim.hunker@hhu.de>
2026-02-03 00:11:50 +0100

    code runs now for train 1, but not for train2

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,19 +1,27 @@
 import TSim.*;
 
 import java.util.Objects;
-import java.util.concurrent.Semaphore;
+import java.util.Map;
+import java.util.HashMap;
+//import java.util.concurrent.Semaphore;
 
 
 
 public class Lab1 {
 
   public Lab1(int speed1, int speed2) {
-// for later, when Classes are finished:
-/*     TrainController controller = new TrainController(...);
-    Thread t1 = new Thread(controller)
-    t1.start(); */
+    Dispatcher dispatcher = new Dispatcher();
 
+    TrainController controller1 = new TrainController(dispatcher, 1, speed1, Direction.FORWARD, TrainMode.WAITING_AT_STATION);
+    Thread t1 = new Thread(controller1);
+    t1.start();
+    
+    // Does not work yet. Missing Rule for x:12,y:9, FORWARD. But then this sensor would trigger if train 1 would drive over it and set the switch twice.
+/*     TrainController controller2 = new TrainController(dispatcher, 2, speed2, Direction.FORWARD, TrainMode.WAITING_AT_STATION);
+    Thread t2 = new Thread(controller2);
+    t2.start(); */
 
+    /* // First Test Senario:
     TSimInterface tsi = TSimInterface.getInstance();
 
     try {
@@ -75,12 +83,10 @@ public class Lab1 {
     catch (CommandException e) {
       e.printStackTrace();    // or only e.getMessage() for the error
       System.exit(1);
-    }
+    }*/
   }
 }
 
-// ------------------------------------------------------------------------
-// Classes for better Handling the trains (instead of using too much if-else)
 
 enum TrainMode {
   RUNNING, WAITING_AT_STATION, WAITING_FOR_OVERTAKE;
@@ -90,8 +96,6 @@ enum Direction {
   FORWARD, BACKWARD;
 }
 
-
-// class for Position in the map. Either Sensor or Switch
 class Position {
   private int xPos;
   private int yPos;
@@ -111,7 +115,6 @@ class Position {
 
   @Override
   public boolean equals(Object obj) {
-
     if (this == obj) return true;
 
     if (obj == null) return false;
@@ -126,12 +129,10 @@ class Position {
   public int hashCode(){
     return Objects.hash(xPos, yPos);
   }
-
 }
 
-// just for the type security, not necessary but nice to have. could be deleted if you want
-// reason: with that you cant compile the code while setting a switch with invalid direction
-// because valid directions are hex:0x01, 0x02 / int: 1, 2 (left, right)
+// just for the type security
+// reason: with that the code won't compile by setting a switch with an invalid switch direction
 enum SwitchDirection {
   LEFT(TSimInterface.SWITCH_LEFT), 
   RIGHT(TSimInterface.SWITCH_RIGHT);
@@ -147,39 +148,28 @@ enum SwitchDirection {
   }
 }
 
-// TODO:
-/* 
-  OvertakeRule
- */
-
-
-// ToDo: in switching Rule: stopping the train and setSpeed again
-// -> TrainController as instance is needed for that
-// -> passing it as a parameter to the method executeRule
+// Todo: OvertakeRule for train that is faster than the other
+// ToDo: in switching Rule: handling blocked trail with semaphor
 
 interface Rule {
   public void executeRule(TrainController controller);
 }
 
-// Switches bei:
-//  17,7
-//  15,9
-//  4,9
-//  3,11
-
 class SwitchRule implements Rule {
 
   private final Position position;
-  private final SwitchDirection direction;
+  private final SwitchDirection switchDirection;
   
-  public SwitchRule(Position position, SwitchDirection direction) {
+  public SwitchRule(Position position, SwitchDirection switchDirection) {
     this.position = position;
-    this.direction = direction;
+    this.switchDirection = switchDirection;
   }
 
   @Override
   public void executeRule(TrainController controller) {
-    controller.setSwitch(position, direction);
+    controller.stop();
+    controller.setSwitch(position, switchDirection);
+    controller.resume();
   }
 }
 
@@ -187,7 +177,6 @@ class StationRule implements Rule {
 
   @Override
   public void executeRule(TrainController controller) {
-        controller.stop();
         controller.waitAtStation();
         controller.reverseDirection();
         controller.resume();
@@ -195,154 +184,190 @@ class StationRule implements Rule {
 }
 
 
-
 class Dispatcher {
 
-    private final Map<Position, Map<Direction, Rule>> rules = new HashMap<>();
+  private final Map<Position, Map<Direction, Rule>> rules = new HashMap<>();
 
-    public Dispatcher() {
-        initRules();
+  public Dispatcher() {
+      initRules();
+  }
+
+  public Rule lookup(Position position, Direction direction) {
+    Map<Direction, Rule> ruleByDirection = rules.get(position);
+
+    if (ruleByDirection == null) {
+      return null;
+    } else {
+      return ruleByDirection.get(direction);
     }
-   
+  }
 
-    public Rule lookup(Position position, Direction direction) {
-      Map<Direction, Rule> byDirection = rules.get(position);
-      if (byDirection == null) return null;
-      return byDirection.get(direction);
-    }
+  private void register(Position position, Direction direction, Rule rule) {
+    rules
+        .computeIfAbsent(position, p -> new HashMap<>())
+        .put(direction, rule);  
+  }
 
-    private void register(Position position, Direction direction, Rule rule) {
-      rules
-          .computeIfAbsent(position, p -> new HashMap<>())
-          .put(direction, rule);  
-    }
-
-    private void initRules() {
-
+  private void initRules() {
     // -------- SWITCH RULES (FORWARD) --------
 
-    register(
-      new Position(14, 7),
+    register(new Position(14, 7),
       Direction.FORWARD,
       new SwitchRule(new Position(17, 7),SwitchDirection.RIGHT)
     );
 
-    register(
-      new Position(18, 9),
+    register(new Position(18, 9),
       Direction.FORWARD, 
       new SwitchRule(new Position(15, 9), SwitchDirection.RIGHT)
     );
 
-    register(
-      new Position(7, 9),
+    register(new Position(7, 9),
       Direction.FORWARD,
       new SwitchRule(new Position(4, 9), SwitchDirection.LEFT)
     );
 
-    register(
-      new Position(1, 10),
+    register(new Position(1, 10),
       Direction.FORWARD,
       new SwitchRule(new Position(3, 11), SwitchDirection.RIGHT)
     );
 
-    // -------- SWITCH RULES (BACKWARDS) --------
+    // -------- SWITCH RULES (BACKWARD) --------
 
-    register(
-      new Position(6, 11), Direction.BACKWARD,
+    register(new Position(6, 11),
+      Direction.BACKWARD,
       new SwitchRule(new Position(3, 11), SwitchDirection.LEFT)
     );
 
-    register(
-      new Position(1, 9), Direction.BACKWARD,
+    register(new Position(1, 9),
+      Direction.BACKWARD,
       new SwitchRule(new Position(4, 9), SwitchDirection.LEFT)
     );
   
-   register(
-        new Position(12, 9), Direction.BACKWARD,
-        new SwitchRule(new Position(15, 9), SwitchDirection.RIGHT)
+    register(new Position(12, 9),
+      Direction.BACKWARD,
+      new SwitchRule(new Position(15, 9), SwitchDirection.RIGHT)
     );
 
-     register(
-        new Position(19, 8), Direction.BACKWARD,
-        new SwitchRule(
-            new Position(17, 7), SwitchDirection.RIGHT
-        )
+    register(new Position(19, 8), 
+      Direction.BACKWARD,
+      new SwitchRule(new Position(17, 7), SwitchDirection.RIGHT)
     );
-
 
     // -------- STATION RULES --------
 
-    // Station top (FORWARD)
-    register(
-        new Position(13, 13), Direction.FORWARD, new StationRule()
+    // Station top - Train 2 (FORWARD)
+    register(new Position(13, 5),
+      Direction.FORWARD,
+      new StationRule()
+    );
+    
+    // Station top - Train 1 (BACKWARD)
+    register(new Position(13, 3),
+      Direction.BACKWARD,
+      new StationRule()
     );
 
-    // Station bottom (BACKWARD)
-    register(
-        new Position(13, 3), Direction.BACKWARD, new StationRule()
+    // Station bottom - Train 1 (FORWARD)
+    register(new Position(13, 13),
+      Direction.FORWARD,
+      new StationRule()
     );
-}
+    // Station bottom - Train 2 (BACKWARD)
+    register(new Position(13, 11),
+      Direction.BACKWARD,
+      new StationRule()
+    );
+  }
 }
 
 class TrainController implements Runnable {
+  private final TSimInterface tsi = TSimInterface.getInstance();
+  private final Dispatcher dispatcher;
   private final int trainId;
+  private int currentSpeed;
   private Direction direction;
   private TrainMode mode;
-  private Dispatcher dispatcher;
-  private TSimInterface tsi = TSimInterface.getInstance(); 
 
-  public TrainController(int id, Direction direction, TrainMode mode) {
+  public TrainController(Dispatcher dispatcher, int id, int speed, Direction direction, TrainMode mode) {
     this.trainId = id;
+    this.dispatcher = dispatcher;
+    this.currentSpeed = speed;
     this.direction = direction;
     this.mode = mode;
-    this.dispatcher = new Dispatcher();
   }
 
-
-  // ToDo:
-  /*       controller.waitAtStation();
-
-        controller.resume(); */
-
-  public void setSwitch(Position position, Direction direction) {
+  public void setSwitch(Position position, SwitchDirection switchDirection) {
     try {
-        tsi.setSwitch(position.getXpos(), position.getYpos(), direction.toTsimDirection());
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
+      tsi.setSwitch(position.getXpos(), position.getYpos(), switchDirection.toTsimDirection());
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
-
-  public void wait(int speed){
-    Thread.sleep(1000 + 20 * Math.abs(speed));
-  } 
 
   public void stop() {
-    //try -catch
-    tsi.setSpeed(trainId,0);
+    try {
+      tsi.setSpeed(trainId, 0);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+  
+  public void sleepAtStation(){
+    try {
+      Thread.sleep(1000 + 20 * Math.abs(this.currentSpeed));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  } 
+
+  public void waitAtStation() {
+    this.stop();
+    this.sleepAtStation();
+    this.mode = TrainMode.WAITING_AT_STATION;
   }
 
-  public void setSpeed(int speed) {
-    //try -catch
-    tsi.setSpeed(speed);
+  // method for overtaking rule
+
+  public void resume() {
+    try {
+      tsi.setSpeed(trainId, this.currentSpeed);
+      this.mode = TrainMode.RUNNING;
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   public void reverseDirection() {
-    //ToDo
+    try {
+      this.currentSpeed *= -1;
+      if (Math.signum(this.currentSpeed) > 0) {
+        this.direction = Direction.FORWARD;
+      } else {
+        this.direction = Direction.BACKWARD;
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
   @Override
   public void run() {
     try {
-        while (true) {          
+        this.resume();
+
+        while (true) {     
           SensorEvent event = tsi.getSensor(trainId);
 
-          Position sensorPosition = new Position(event.getXpos, event.getYpos);
+          if (event.getStatus() != SensorEvent.ACTIVE) continue;
+
+          Position sensorPosition = new Position(event.getXpos(), event.getYpos());
 
           Rule rule = dispatcher.lookup(sensorPosition, direction);
-          
-          rule.executeRule();
+          if (rule == null) continue;
+          rule.executeRule(this);
       }
+    } catch (Exception e) {
+      e.printStackTrace();
     }
   }
-
 }
